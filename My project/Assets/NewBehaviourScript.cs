ec8e050dbf9e94557e86d31cfc46b71026c39580
frimen7 <99037840+frimen7@users.noreply.github.com>
2022-02-13 21:29:19 +0500

    Update NewBehaviourScript.cs

--- My project/Assets/NewBehaviourScript.cs
+++ My project/Assets/NewBehaviourScript.cs
@@ -6,6 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     // СЕРГЕЙ ЛУЧШИЙ РАЗРАБОТЧИК ИГР НА ЮНИТИ!!!
     // Мой коментарий
+    // Не знаю я
     // Start is called before the first frame update
     void Start()
     {
